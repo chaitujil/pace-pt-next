340a879e9014d9eb73e6c427831223d7e767dec3
Krishna Yalamanchili <chaitujil@users.noreply.github.com>
2019-10-08 23:46:14 -0700

    Create Meta.js

---
+++ components/Meta.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+
+const Meta = () => (
+        <Head>
+          <title key="title">Pace Physical Therapy</title>
+
+          <meta name="google-site-verification" content="5NZb-fC8gGbIHpjiUkdWUDhPYC0hd-bcezSe5Ac0I2w" />
+        </Head>
+ );
+
+ export default Meta;
